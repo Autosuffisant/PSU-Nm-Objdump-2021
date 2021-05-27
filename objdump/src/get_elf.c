f85923217e39310e9d671f20fad38660f5d9d994
Prism <63168542+Autosuffisant@users.noreply.github.com>
2021-05-28 00:20:29 +0200

    Create get_elf.c

---
+++ objdump/src/get_elf.c
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2021
+** nm
+** File description:
+** Recreate the nm function
+*/
+
+#include "objdump.h"
+
+int file_get_content(elf_data_t *elf)
+{
+    if ((elf->fd = (open(elf->file_path, O_RDONLY)))
+    == -1)
+        return fprintf(stderr, "my_objdump: '%s': No such file\n",
+        elf->file_path);
+    if ((elf->file_size = lseek((elf->fd), 0, SEEK_END))
+    == -1) {
+        close(elf->fd);
+        return fprintf(stderr, "my_objdump: '%s': file format not recognized\n",
+        elf->file_path);
+    }
+    elf->data = mmap(NULL, elf->file_size, PROT_READ, MAP_SHARED, elf->fd, 0);
+    if (elf->data == MAP_FAILED && elf->file_size > 0) {
+        close(elf->fd);
+        return fprintf(stderr, "my_objdump: '%s': file format not recognized\n",
+        elf->file_path);
+    }
+    if (elf->file_nbr > 1)
+        printf("\n%s:\n", elf->file_path);
+    return 0;
+}
